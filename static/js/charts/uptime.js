f62407270b85fa7063083b9566da5649f155a8c1
yo-ru <yoru@its.moe>
2023-04-11 04:21:50 -0500

    implement our first chart!

---
+++ static/js/charts/uptime.js
@@ -0,0 +1,65 @@
+(async function updateSeries() {
+    let response = await fetch("/api/servers/average")
+    let json = await response.json()
+
+    let series = [];
+    Object.entries(json).forEach(([mirror, data]) => {
+        series.push({
+            name: mirror,
+            data: [((data.download.average.uptime + data.search.average.uptime) / 2) * 100]
+        })
+    });
+
+    chart.updateSeries(series)
+    setTimeout(updateSeries, 60 * 1000 * 5)
+})()
+
+var options = {
+    theme: {
+        mode: 'dark', 
+        palette: 'palette1', 
+        monochrome: {
+            enabled: true,
+            color: '#7171ff',
+            shadeTo: 'light',
+            shadeIntensity: 1
+        },
+    },
+    series: [],
+    chart: {
+        width: 500,
+        background: "#1E1E2E",
+        fontFamily: 'Uni Sans',
+        type: 'bar',
+        zoom: {
+            enabled: false
+        },
+        toolbar: {
+            show: false
+        },
+    },
+    dataLabels: {
+        enabled: false
+    },
+    title: {
+        text: 'Uptime % to Date',
+        align: 'center'
+    },
+    yaxis: {
+        max: 100,
+        min: 90
+    },
+    xaxis: {
+        categories: ['Uptime'],
+    },
+    tooltip: {
+        y: {
+            formatter: function(value, options) {
+                return `${value}%`
+            }
+        }
+    }
+}
+
+var chart = new ApexCharts(document.querySelector("#uptime-chart"), options)
+chart.render()
