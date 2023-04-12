acbae1b5eaa716915b02769e09577f95e437bc24
HorizonCode <horizoncode88@gmail.com>
2023-04-12 13:19:52 +0200

    clean up charts code, add other latency charts

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,8 +1,176 @@
-const initSeries = async (apiUrl, json) => {
+const chartSettings = {
+  latency: {
+    theme: {
+      mode: 'dark',
+      palette: 'palette8',
+      monochrome: {
+        enabled: false,
+        color: '#7171ff',
+        shadeTo: 'light',
+        shadeIntensity: 1
+      },
+    },
+    series: [],
+    chart: {
+      width: "200%",
+      background: "#1E1E2E",
+      fontFamily: 'Uni Sans',
+      type: 'line',
+      zoom: {
+        enabled: false
+      },
+      toolbar: {
+        show: false
+      },
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    dataLabels: {
+      enabled: false
+    },
+    title: {
+      text: '',
+      align: 'center'
+    },
+    xaxis: {
+      type: 'category',
+    },
+    tooltip: {
+      inverseOrder: true,
+      y: {
+        formatter: function (value, options) {
+          return `${value}ms`
+        }
+      }
+    },
+    responsive: [{
+      breakpoint: 1024,
+      options: {
+        chart: {
+          width: "140%"
+        }
+      }
+    }],
+    legend: {
+      onItemClick: {
+        toggleDataSeries: false
+      }
+    }
+  },
+  uptime: {
+    theme: {
+      mode: 'dark',
+      palette: 'palette8',
+      monochrome: {
+        enabled: false,
+        color: '#7171ff',
+        shadeTo: 'light',
+        shadeIntensity: 1
+      },
+    },
+    series: [],
+    chart: {
+      width: "200%",
+      background: "#1E1E2E",
+      fontFamily: 'Uni Sans',
+      type: 'bar',
+      zoom: {
+        enabled: false
+      },
+      toolbar: {
+        show: false
+      },
+    },
+    dataLabels: {
+      enabled: false
+    },
+    title: {
+      text: 'Uptime % (all time)',
+      align: 'center'
+    },
+    yaxis: {
+      max: 100,
+      min: 50,
+      decimalsInFloat: 0,
+
+      labels: {
+        formatter: function (value, index) {
+          return `${value.toFixed(2)}%`
+        }
+      }
+    },
+    xaxis: {
+      categories: [''],
+    },
+    tooltip: {
+      y: {
+        formatter: function (value, options) {
+          return `${value.toFixed(2)}%`
+        }
+      }
+    },
+    responsive: [{
+      breakpoint: 1024,
+      options: {
+        chart: {
+          width: "140%"
+        }
+      }
+    }],
+    legend: {
+      onItemClick: {
+        toggleDataSeries: false
+      }
+    }
+  }
+};
+
+const charts = [];
+
+class LatencyChart {
+
+  constructor(selector, latencyType, title) {
+    this.latencyType = latencyType;
+    this.title = title;
+    this.chart = new ApexCharts(document.querySelector(selector), chartSettings.latency);
+    this.chart.render()
+    
+  }
+
+  updateSeries(json) {
+    let series = []
+    console.log("updating")
+    Object.entries(json).forEach(([mirror, data]) => {
+      let categories = data[this.latencyType].time.splice(data[this.latencyType].latency.length - 9, data.search.latency.length);
+      categories = categories.map((time) => {
+        return moment(new Date(time * 1000)).fromNow()
+      });
+
+      const dataa = data[this.latencyType].latency.splice(data[this.latencyType].latency.length - 9, data.search.latency.length);
+      series.push({
+        name: mirror,
+        type: 'line',
+        data: dataa
+      });
+
+      this.chart.updateSeries(series)
+      this.chart.updateOptions({
+        title: {
+          text: this.title,
+          align: 'center'
+        },
+        xaxis: {
+          categories: categories
+        }
+      })
+    });
+  }
+}
+
+const initSeries = async (apiUrl, json, allDataJson) => {
   updateUptimeSeries(json);
-  let allResponse = await fetch(`${apiUrl}/servers`);
-  let allDataJson = await allResponse.json();
-  updateSearchLatencySeries(allDataJson);
+  charts.forEach(chart => chart.updateSeries(allDataJson))
   setTimeout(() => reloadSeries(apiUrl), 60 * 1000 * 1)
 }
 
@@ -12,7 +180,7 @@ const reloadSeries = async (apiUrl) => {
   let allResponse = await fetch(`${apiUrl}/servers`);
   let allDataJson = await allResponse.json();
   updateUptimeSeries(recentDataJson);
-  updateSearchLatencySeries(allDataJson);
+  charts.forEach(chart => chart.updateSeries(allDataJson))
   setTimeout(() => reloadSeries(apiUrl), 60 * 1000 * 1)
 }
 
@@ -27,159 +195,9 @@ const updateUptimeSeries = (json) => {
   uptimeChart.updateSeries(series.sort((a, b) => a.data[0] - b.data[0]))
 };
 
-const updateSearchLatencySeries = (json) => {
-  let series = []
-  Object.entries(json).forEach(([mirror, data]) => {
-    let categories = data.search.time.splice(data.search.latency.length - 9, data.search.latency.length);
-    categories = categories.map((time) => {
-      return moment(new Date(time * 1000)).fromNow()
-    });
-
-    const dataa = data.search.latency.splice(data.search.latency.length - 9, data.search.latency.length);
-    series.push({
-      name: mirror,
-      type: 'line',
-      data: dataa
-    });
-
-    searchChart.updateSeries(series)
-    searchChart.updateOptions({
-      xaxis: {
-        categories: categories
-      }
-    })
-  });
-}
-
-var uptimeOptions = {
-  theme: {
-    mode: 'dark',
-    palette: 'palette8',
-    monochrome: {
-      enabled: false,
-      color: '#7171ff',
-      shadeTo: 'light',
-      shadeIntensity: 1
-    },
-  },
-  series: [],
-  chart: {
-    width: "200%",
-    background: "#1E1E2E",
-    fontFamily: 'Uni Sans',
-    type: 'bar',
-    zoom: {
-      enabled: false
-    },
-    toolbar: {
-      show: false
-    },
-  },
-  dataLabels: {
-    enabled: false
-  },
-  title: {
-    text: 'Uptime % (all time)',
-    align: 'center'
-  },
-  yaxis: {
-    max: 100,
-    min: 50,
-    decimalsInFloat: 0,
-
-    labels: {
-      formatter: function (value, index) {
-        return `${value.toFixed(2)}%`
-      }
-    }
-  },
-  xaxis: {
-    categories: [''],
-  },
-  tooltip: {
-    y: {
-      formatter: function (value, options) {
-        return `${value.toFixed(2)}%`
-      }
-    }
-  },
-  responsive: [{
-    breakpoint: 1024,
-    options: {
-      chart: {
-        width: "140%"
-      }
-    }
-  }],
-  legend: {
-    onItemClick: {
-      toggleDataSeries: false
-    }
-  }
-}
-
-var searchOptions = {
-  theme: {
-    mode: 'dark',
-    palette: 'palette8',
-    monochrome: {
-      enabled: false,
-      color: '#7171ff',
-      shadeTo: 'light',
-      shadeIntensity: 1
-    },
-  },
-  series: [],
-  chart: {
-    width: "200%",
-    background: "#1E1E2E",
-    fontFamily: 'Uni Sans',
-    type: 'line',
-    zoom: {
-      enabled: false
-    },
-    toolbar: {
-      show: false
-    },
-  },
-  stroke: {
-    curve: 'smooth'
-  },
-  dataLabels: {
-    enabled: false
-  },
-  title: {
-    text: 'Search latency (last hour)',
-    align: 'center'
-  },
-  xaxis: {
-    type: 'category',
-  },
-  tooltip: {
-    inverseOrder: true,
-    y: {
-      formatter: function (value, options) {
-        return `${value}ms`
-      }
-    }
-  },
-  responsive: [{
-    breakpoint: 1024,
-    options: {
-      chart: {
-        width: "140%"
-      }
-    }
-  }],
-  legend: {
-    onItemClick: {
-      toggleDataSeries: false
-    }
-  }
-}
-
-var uptimeChart = new ApexCharts(document.querySelector("#uptime-chart"), uptimeOptions)
+var uptimeChart = new ApexCharts(document.querySelector("#uptime-chart"), chartSettings.uptime)
 uptimeChart.render()
 
-var searchChart = new ApexCharts(document.querySelector("#search-chart"), searchOptions)
-searchChart.render()
+charts.push(new LatencyChart("#search-chart", "search", "Search latency (last hour)"));
+charts.push(new LatencyChart("#download-chart", "download", "Download latency (last hour)"));
+charts.push(new LatencyChart("#status-chart", "status", "Status latency (last hour)"));
--- static/js/script.js
+++ static/js/script.js
@@ -6,27 +6,26 @@ const setServer = async (url) => {
 
 /* peep the cursed js */
 const resetPage = async () => {
-  let id = window.setTimeout(() => {}, 0)
+  let id = window.setTimeout(() => { }, 0)
   while (id--) window.clearTimeout(id)
-  
+
   setTimeout(() => document.getElementById("center").classList.remove("aos-animate"), 10)
   setTimeout(() => document.querySelector(".mirror-list").innerHTML = '', 500)
   setTimeout(() => document.getElementById("center").classList.add("aos-animate"), 500)
 }
 
 const buildPage = async (apiUrl) => {
-  let resp = await fetch(`${apiUrl}/servers/average`)
-  let upResp = await fetch(`${apiUrl}/servers`)
-  let json = await resp.json();
-  let upJson = await upResp.json()
+  const resp = await fetch(`${apiUrl}/servers/average`)
+  const upResp = await fetch(`${apiUrl}/servers`)
+  const avgJson = await resp.json();
+  const upJson = await upResp.json()
 
   // sort json by resp time
-  json = Object.fromEntries(
-    Object.entries(json).sort(([, a], [, b]) => (a.search.average.latency - b.search.average.latency))
+  const sortedJson = Object.fromEntries(
+    Object.entries(avgJson).sort(([, a], [, b]) => (a.search.average.latency - b.search.average.latency))
   );
 
-  let index = 6;
-  Object.entries(json).forEach(([mirror, _data]) => {
+  Object.entries(sortedJson).forEach(([mirror, _data]) => {
     const mirrorCard = document.createElement('div');
     mirrorCard.classList.add('mirror');
 
@@ -35,32 +34,37 @@ const buildPage = async (apiUrl) => {
     mirrorText.href = `https://${mirror}`;
     mirrorText.target = "_blank";
     mirrorCard.appendChild(mirrorText);
-    
+
     const up = [upJson[mirror].status.uptime.slice(-1)[0], upJson[mirror].search.uptime.slice(-1)[0], upJson[mirror].download.uptime.slice(-1)[0]]
     const upTotal = up.reduce((a, b) => a + b, 0)
     const mirrorUpDown = document.createElement('h1');
-    if (upTotal === 3) {
-      mirrorCard.innerHTML = '<i class="fa-solid fa-circle-up fa-2x1" style="color: #90EE90"></i> ' + mirrorCard.innerHTML
-      mirrorUpDown.innerHTML = '<span>UP</span>'
-    } else if (upTotal === 0) {
-      mirrorCard.innerHTML = '<i class="fa-solid fa-circle-down fa-2x1" style="color: #FF7276"></i> ' + mirrorCard.innerHTML
-      mirrorUpDown.innerHTML = '<span>DOWN</span>'
-    } else {
-      mirrorCard.innerHTML = '<i class="fa-solid fa-circle-exclamation fa-2x1" style="color: #FFC600"></i> ' + mirrorCard.innerHTML
-      mirrorUpDown.innerHTML = '<span>DEGRADED</span>'
-      
-      const degradedEndpoints = document.createElement('h3')
-      degradedEndpoints.classList.add('degraded')
-      if (up[0] === 0) degradedEndpoints.innerHTML += '<span>Status</span>' 
-      if (up[1] === 0) degradedEndpoints.innerHTML += '<span>Search</span>'
-      if (up[2] === 0) degradedEndpoints.innerHTML += '<span>Download</span>'
-      mirrorCard.appendChild(degradedEndpoints)
+
+    switch (upTotal) {
+      case 0:
+        mirrorCard.innerHTML = '<i class="fa-solid fa-circle-down fa-2x1" style="color: #FF7276"></i> ' + mirrorCard.innerHTML
+        mirrorUpDown.innerHTML = '<span>DOWN</span>'
+        break;
+      case 3:
+        mirrorCard.innerHTML = '<i class="fa-solid fa-circle-up fa-2x1" style="color: #90EE90"></i> ' + mirrorCard.innerHTML
+        mirrorUpDown.innerHTML = '<span>UP</span>'
+        break;
+      default:
+        mirrorCard.innerHTML = '<i class="fa-solid fa-circle-exclamation fa-2x1" style="color: #FFC600"></i> ' + mirrorCard.innerHTML
+        mirrorUpDown.innerHTML = '<span>DEGRADED</span>'
+
+        const degradedEndpoints = document.createElement('h3')
+        degradedEndpoints.classList.add('degraded')
+        if (up[0] === 0) degradedEndpoints.innerHTML += '<span>Status</span>'
+        if (up[1] === 0) degradedEndpoints.innerHTML += '<span>Search</span>'
+        if (up[2] === 0) degradedEndpoints.innerHTML += '<span>Download</span>'
+        mirrorCard.appendChild(degradedEndpoints)
+        break;
     }
+
     mirrorCard.appendChild(mirrorUpDown)
     document.querySelector('.mirror-list').appendChild(mirrorCard);
-    index++;
   });
-  initSeries(apiUrl, json);
+  initSeries(apiUrl, sortedJson, upJson);
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
