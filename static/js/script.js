7e24f84badbf6603ef619d8f10eba3029788d56e
yo-ru <yoru@its.moe>
2023-04-11 23:53:59 -0500

    implement up/down/degraded status

--- static/js/script.js
+++ static/js/script.js
@@ -18,6 +18,28 @@ document.addEventListener('DOMContentLoaded', async () => {
     mirrorText.href = `https://${mirror}`;
     mirrorText.target = "_blank";
     mirrorCard.appendChild(mirrorText);
+    
+    const up = [upJson[mirror].status.uptime.slice(-1)[0], upJson[mirror].search.uptime.slice(-1)[0], upJson[mirror].download.uptime.slice(-1)[0]]
+    const upTotal = up.reduce((a, b) => a + b, 0)
+    const mirrorUpDown = document.createElement('h1');
+    if (upTotal === 3) {
+      mirrorCard.innerHTML = '<i class="fa-solid fa-circle-up fa-2x1" style="color: #90EE90"></i> ' + mirrorCard.innerHTML
+      mirrorUpDown.innerHTML = '<span>UP</span>'
+    } else if (upTotal === 0) {
+      mirrorCard.innerHTML = '<i class="fa-solid fa-circle-down fa-2x1" style="color: #FF7276"></i> ' + mirrorCard.innerHTML
+      mirrorUpDown.innerHTML = '<span>DOWN</span>'
+    } else {
+      mirrorCard.innerHTML = '<i class="fa-solid fa-circle-exclamation fa-2x1" style="color: #FFC600"></i> ' + mirrorCard.innerHTML
+      mirrorUpDown.innerHTML = '<span>DEGRADED</span>'
+      
+      const degradedEndpoints = document.createElement('h3')
+      degradedEndpoints.classList.add('degraded')
+      if (up[0] === 0) degradedEndpoints.innerHTML += '<span>Status</span>' 
+      if (up[1] === 0) degradedEndpoints.innerHTML += '<span>Search</span>'
+      if (up[2] === 0) degradedEndpoints.innerHTML += '<span>Download</span>'
+      mirrorCard.appendChild(degradedEndpoints)
+    }
+    mirrorCard.appendChild(mirrorUpDown)
     mirrorCard.dataset.aos = "fade-in";
     mirrorCard.dataset.aosDelay = index * 100;
     document.getElementById('mirror-list').appendChild(mirrorCard);
